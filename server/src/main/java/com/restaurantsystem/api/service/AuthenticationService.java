242564a7892a7dabf10587afa401c45777a953ed
Owen <kemp.56@wright.edu>
2024-03-06 20:54:15 -0500

    Autowire

--- server/src/main/java/com/restaurantsystem/api/service/AuthenticationService.java
+++ server/src/main/java/com/restaurantsystem/api/service/AuthenticationService.java
@@ -7,12 +7,8 @@ import com.restaurantsystem.api.repos.WorkerRepository;
 
 @Service
 public class AuthenticationService {
-    WorkerRepository userRepository;
-
     @Autowired
-    public AuthenticationService(WorkerRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+    WorkerRepository userRepository;
 
     public String login(String username, String password) {
         return userRepository.getToken(username, password);
