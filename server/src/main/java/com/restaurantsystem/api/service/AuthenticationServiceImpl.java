d599d991cef9086cf590a79c402365b5fcf8c585
Owen <kemp.56@wright.edu>
2024-03-13 12:46:58 -0400

    Unit tests

--- server/src/main/java/com/restaurantsystem/api/service/AuthenticationServiceImpl.java
+++ server/src/main/java/com/restaurantsystem/api/service/AuthenticationServiceImpl.java
@@ -3,6 +3,8 @@ package com.restaurantsystem.api.service;
 import java.util.Date;
 import java.util.Optional;
 
+import javax.crypto.SecretKey;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -12,7 +14,9 @@ import com.restaurantsystem.api.repos.WorkerRepository;
 import com.restaurantsystem.api.service.interfaces.AuthenticationService;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtParser;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
@@ -26,7 +30,6 @@ public class AuthenticationServiceImpl implements AuthenticationService {
         Optional<Worker> worker = workerRepository.findByUsername(username);
         if (worker.isEmpty())
             return Optional.empty();
-        System.out.println(worker.get().getPasswordHash());
         if (!passwordEncoder.matches(password, worker.get().getPasswordHash()))
             return Optional.empty();
         String token = generateToken(username);
@@ -38,6 +41,10 @@ public class AuthenticationServiceImpl implements AuthenticationService {
      * How long the token lasts until expiration
      */
     private static final long TOKEN_ACCESS_TIME = 60 * 60 * 24 * 1000; // 24 hours
+    // TODO put the key somewhere else
+    private static final String SECRET_KEY = "noijgnweiuongqeonhOHIOUHEGIUEhgiuSHeghshoGSEBUIGsG";
+    private static final SecretKey KEY = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+    private static final JwtParser JWT_PARSER = Jwts.parser().verifyWith(KEY).build();
 
     /**
      * Generates a token
@@ -48,12 +55,12 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     private static String generateToken(String username) {
         Date currentDate = new Date();
         Date expirationDate = new Date(currentDate.getTime() + TOKEN_ACCESS_TIME);
-        return Jwts.builder().subject(username).expiration(expirationDate).compact();
+        return Jwts.builder().subject(username).expiration(expirationDate).signWith(KEY).compact();
     }
 
     private static boolean isValidToken(String token) {
         try {
-            Claims claims = Jwts.parser().build().parseUnsecuredClaims(token).getPayload();
+            Claims claims = JWT_PARSER.parseSignedClaims(token).getPayload();
             if (!claims.getExpiration().after(new Date()))
                 return false;
             return true;
--- server/src/test/java/com/restaurantsystem/api/service/AuthenticationServiceTest.java
+++ server/src/test/java/com/restaurantsystem/api/service/AuthenticationServiceTest.java
@@ -1,23 +1,51 @@
 package com.restaurantsystem.api.service;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.context.transaction.BeforeTransaction;
 
+import com.restaurantsystem.api.data.Worker;
+import com.restaurantsystem.api.repos.WorkerRepository;
 import com.restaurantsystem.api.service.interfaces.AuthenticationService;
+
+import jakarta.transaction.Transactional;
 
 @SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
 public class AuthenticationServiceTest {
     @Autowired
+    WorkerRepository workerRepository;
+
+    @Autowired
     AuthenticationService authenticationService;
 
+    BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    @BeforeTransaction
+    void populate() {
+        Worker worker1 = new Worker();
+        worker1.setUsername("test");
+        worker1.setPasswordHash(passwordEncoder.encode("test"));
+        workerRepository.save(worker1);
+    }
+
     @Test
+    @Transactional
     void login() {
-        assertTrue(authenticationService.login("LOLNOPE", "").isEmpty());
-        assertEquals(authenticationService.login("test", "").get(), "hufhiohwef");
+        assertTrue(authenticationService.login("jlgebg", "uiguef").isEmpty());
+        Optional<String> t = authenticationService.login("test", "test");
+        assertTrue(t.isPresent());
+        authenticate(t.get());
+    }
+
+    void authenticate(String token) {
+        assertTrue(authenticationService.authenticate(token).isPresent());
+        assertTrue(authenticationService.authenticate("hqe8fh").isEmpty());
     }
 }
